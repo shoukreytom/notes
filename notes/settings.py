94e82552cd122f24fbd1029dcc9f5d5694c266c2
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2020-12-13 11:30:10 +0200

    added addmynote.herokuapp.com to allowed hosts + add STATIC_ROOT var

--- notes/settings.py
+++ notes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '+)nh!^gk-=ot-y9_xh#zx(v=l2m7-6tzh!$^iaa07kak%4y+vo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['addmynote.herokuapp.com', '127.0.0.1']
 
 AUTH_USER_MODEL = 'account.Account'
 
@@ -122,5 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+import os
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
