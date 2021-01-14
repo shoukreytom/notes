c05c90877c3c14458db7dea575bedee3ba72d8c1
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2021-01-14 15:33:29 +0200

    served static files

--- notes/urls.py
+++ notes/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +12,5 @@ urlpatterns = [
     path('notes/api/v1/', include('home.api.urls')),
     path('accounts/api/v1/', include('account.api.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
