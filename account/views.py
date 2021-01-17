bbab4952657cb9772802f75ae019c4e18d07c2d5
abdulshak1999 <abdulshakoor.abdulazeem@gmail.com>
2021-01-17 17:53:23 +0200

    :repeat: :repeat: :zap: :zap: fixed signin redirect

--- account/views.py
+++ account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
+from django.urls import reverse
 from .models import Account
 from home.models import Note
 
@@ -21,14 +22,19 @@ def signup(request):
 
 @csrf_exempt
 def signin(request):
+    next_url = request.GET.get('next', '')
     if request.method == 'POST':
         email = request.POST.get('email', None)
         password = request.POST.get('password', None)
         user = authenticate(email=email, password=password)
         if user is not None:
             login(request, user)
+            # TODO: parse path to redirect user to specified path
+            next_url = request.POST.get('next')
+            if next_url != '':
+                return redirect(next_url)
             return redirect('home')
-    return render(request, 'account/signin.html')
+    return render(request, 'account/signin.html', {'next': next_url})
 
 @login_required
 def profile(request):
